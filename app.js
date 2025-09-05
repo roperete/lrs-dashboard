b958dc45d5ba660738648c985a13abb061670cc5
roperete <roperizate@gmail.com>
2025-09-05 09:34:02 +0200

    modified:   app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // document.addEventListener('DOMContentLoaded', () => {
-
+// Git test changes!
 //     let map = L.map('map').setView([46.6, 2.5], 3);
 //     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom:19 }).addTo(map);
 
